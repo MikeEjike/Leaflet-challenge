e5d01ea3c4e207919ca018c543f235f7f3f5a7db
Michael Ejike <mikesoccer12@gmail.com>
2021-02-06 16:31:32 -0500

    data retrieval

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,25 +1,30 @@
-let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let url =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(url).then(createEarthQuake);
 
-// d3.json(url, function(data){
-//     console.log(data)
-
-// })
-
 function createEarthQuake(data) {
-  console.log(data);
-  
-// //   let myMap = createMap();
-  
-// //   points = data.map((d) => [
-// //     d.location.coordinates[1],
-// //     d.location.coordinates[0],
-// //   ]);
-  
-// //   console.log(points)
-  
-// //   var heat = L.heatLayer(points, { radius: 20, blur: 35 })
-// //   heat.addTo(myMap);
+  console.log(data.features);
+  //   console.log(data.features[0].geometry.coordinates);
+  let locationData = data.features;
+  let lng = [];
+  let lat = [];
+  locationData.forEach((item) => {
+    let coordinates = item.geometry.coordinates;
+    lng.push(coordinates[0]);
+    lat.push(coordinates[1]);
+    //   console.log(coordinates[1])
+  });
 
+  // //   let myMap = createMap();
+
+  // //   points = data.map((d) => [
+  // //     d.location.coordinates[1],
+  // //     d.location.coordinates[0],
+  // //   ]);
+
+  // //   console.log(points)
+
+  // //   var heat = L.heatLayer(points, { radius: 20, blur: 35 })
+  // //   heat.addTo(myMap);
 }
