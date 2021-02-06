1e24f10fbc8fda0a6d6a9d56fa2c905f512e91e0
Michael Ejike <mikesoccer12@gmail.com>
2021-02-06 18:48:03 -0500

    added item to gitignore

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,19 +1,6 @@
 let url =
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// let lightmap = L.tileLayer(
-//   "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-//   {
-//     attribution:
-//       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: "light-v10",
-//     accessToken: API_KEY,
-//   }
-// );
-
-// center: [34.0522, -118.2437],
-// zoom: 8
 let myMap = L.map("map").setView([37.0902, -95.7129], 4);
 
 L.tileLayer(
@@ -31,25 +18,8 @@ L.tileLayer(
 
 d3.json(url).then(createEarthQuake);
 
-function colorGrade(magnitude) {
-  if (rating < 1) {
-    return "#74c476";
-  } else if (rating < 2) {
-    return "#ffffb2";
-  } else if (rating < 3) {
-    return "#fecc5c";
-  } else if (rating < 4) {
-    return "#fd8d3c";
-  } else if (rating < 5) {
-    return "#f03b20";
-  } else {
-    return "#bd0026";
-  }
-}
-
 function createEarthQuake(data) {
   console.log(data.features);
-  //   console.log(data.features[0].geometry.coordinates);
   let locationData = data.features;
 
   locationData.forEach((item) => {
@@ -63,7 +33,7 @@ function createEarthQuake(data) {
     circle
       .bindPopup(
         "<h3>" +
-          item.properties.place + //   feature.properties.place +
+          item.properties.place + 
           "</h3><hr></p><p> Magnitude: " +
           item.properties.mag +
           "</p><p> Coordinates (lat, lng): " +
