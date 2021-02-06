0df68d417322c26731ab2b413d2e179a1fce5f54
Michael Ejike <mikesoccer12@gmail.com>
2021-02-06 18:28:47 -0500

    commit

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -31,6 +31,22 @@ L.tileLayer(
 
 d3.json(url).then(createEarthQuake);
 
+function colorGrade(magnitude) {
+  if (rating < 1) {
+    return "#74c476";
+  } else if (rating < 2) {
+    return "#ffffb2";
+  } else if (rating < 3) {
+    return "#fecc5c";
+  } else if (rating < 4) {
+    return "#fd8d3c";
+  } else if (rating < 5) {
+    return "#f03b20";
+  } else {
+    return "#bd0026";
+  }
+}
+
 function createEarthQuake(data) {
   console.log(data.features);
   //   console.log(data.features[0].geometry.coordinates);
@@ -40,28 +56,50 @@ function createEarthQuake(data) {
   let lat = [];
   locationData.forEach((item) => {
     let coordinate = item.geometry.coordinates;
+
+    let intensity = item.properties.mag;
     let coordinates = [coordinate[1], coordinate[0]];
+
+    let circle = L.circle(coordinates, intensity*35000, {color: colorGrade(intensity)})
+    
+    
+    circle.bindPopup(
+        "<h3>" +
+        item.properties.place //   feature.properties.place +
+        + "</h3><hr></p><p> Magnitude: " + item.properties.mag  + "</p><p> Coordinates (lat, lng): " + coordinates + "</p>"
+        ).openPopup();
+        
+    circle.addTo(myMap)
+
     lng.push(coordinate[0]);
     lat.push(coordinate[1]);
     latLng.push(coordinates);
   });
   console.log(latLng);
 
-  function colorGrade{
-  if (rating <= 1) {
-    return "red";
+  function colorGrade(magnitude) {
+    if (magnitude < 1) {
+      return "#74c476";
+    } else if (magnitude < 2) {
+      return "#ffffb2";
+    } else if (magnitude < 3) {
+      return "#fecc5c";
+    } else if (magnitude < 4) {
+      return "#fd8d3c";
+    } else if (magnitude < 5) {
+      return "#f03b20";
+    } else if (magnitude > 5) {
+      return "#bd0026";
     }
-    else if (rating === 3) and (rating === 3); {
-        return "orange";
-    } 
-    // else if (rating === 5){
-    //     return = "orange"
-    // }
-    // else{};
-    // }
-//   var blueMarker = L.AwesomeMarkers.icon({
-//     markerColor: customColor,
-//   });
+  }
+  // else if (rating === 5){
+  //     return = "orange"
+  // }
+  // else{};
+  // }
+  //   var blueMarker = L.AwesomeMarkers.icon({
+  //     markerColor: customColor,
+  //   });
 
   //   var bikeMarkers = [];
 
